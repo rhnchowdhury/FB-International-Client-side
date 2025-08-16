a1fc4344b212ad45128c30cd5f68cbdafc632dd5
rhnchowdhury <rhnchdri@gmail.com>
2025-08-16 13:08:18 +0600

    expense history create

--- src/components/pages/Expenses/ExpenseHistory.jsx
+++ src/components/pages/Expenses/ExpenseHistory.jsx
@@ -1,34 +1,51 @@
+import { useEffect, useState } from "react";
+
 const ExpenseHistory = () => {
+  const [allExpenses, setAllExpenses] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/expense")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllExpenses(data);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
+
   return (
     <div>
       <div className="overflow-x-auto">
         <table className="table ">
           <thead>
             <tr>
-              <th className="border-2"></th>
-              <th className="border-2">Name</th>
-              <th className="border-2">Email</th>
-              <th className="border-2"> Role</th>
-              <th className="border-2">Make Admin</th>
-              <th className="border-2">Delete</th>
+              <th className="border-2 text-[#D8FFFB]">No.</th>
+              <th className="border-2 text-[#D8FFFB]">Title</th>
+              <th className="border-2 text-[#D8FFFB]">Amount</th>
+              <th className="border-2 text-[#D8FFFB]">Category</th>
+              <th className="border-2 text-[#D8FFFB]">Date</th>
+              <th className="border-2 text-[#D8FFFB]">Option</th>
+              <th className="border-2 text-[#D8FFFB]">Delete</th>
             </tr>
           </thead>
-          {/* {allExpenses.map((usr, i) => ( */}
-          <tbody key={usr._id}>
-            <tr>
-              <th className="border-2">{i + 1}</th>
-              <td className="border-2">{usr.name}</td>
-              <td className="border-2"> {usr.email}</td>
-              <td className="border-2">{usr.role}</td>
-              <td className="border-2">
-                <button className="btn btn-xs">Make Admin</button>
-              </td>
-              <td className="border-2">
-                <button className="btn btn-warning  btn-outline">Delete</button>
-              </td>
-            </tr>
-          </tbody>
-          {/* ))} */}
+          {allExpenses.map((expense, i) => (
+            <tbody key={expense._id}>
+              <tr>
+                <th className="border-2 text-[#D8FFFB]">{i + 1}</th>
+                <td className="border-2 text-[#D8FFFB]">{expense.title}</td>
+                <td className="border-2 text-[#D8FFFB]"> {expense.amount}</td>
+                <td className="border-2 text-[#D8FFFB]">{expense.select}</td>
+                <td className="border-2 text-[#D8FFFB]">{expense.date}</td>
+                <td className="border-2">
+                  <button className="btn btn-xs">Edit</button>
+                </td>
+                <td className="border-2">
+                  <button className="btn text-[#00DAC6] hover:text-black btn-outline hover:bg-[#00DAC6] border-[#00DAC6]">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          ))}
         </table>
       </div>
     </div>
